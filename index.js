c30980c9fadc3c509c0e5cfd032b92fe667d7057
mrkataei <kouroshinfo96@gmail.com>
2023-07-24 18:25:43 +0330

    use cors for call api in front side

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const port = 3001;
 const path = require("path");
 const pathToFile = path.resolve("./data.json");
 
+const cors = require("cors");
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 const fs = require("fs");
 const getResources = () => JSON.parse(fs.readFileSync(pathToFile));
 app.get("/", (req, res) => {
